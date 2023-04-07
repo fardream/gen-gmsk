f6d95ea4723030600ee6a04ece09f004763c8d7f
Chao Xu <fardream@users.noreply.github.com>
2023-04-07 06:57:59 +0000

    auto generate functions

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"flag"
+	"io"
 	"log"
 	"os"
 	"path"
@@ -21,6 +22,27 @@ func orPanic(err error) {
 func getOrPanic[T any](a T, err error) T {
 	orPanic(err)
 	return a
+}
+
+func builderToFile(outputDir, outFile string, m *MosekH, c *OutputConfig, buildFunc func(*MosekH, *OutputConfig, io.Writer) error) {
+	var fileContent bytes.Buffer
+	orPanic(buildFunc(m, c, &fileContent))
+	formattedContent, err := format.Source(fileContent.Bytes(), format.Options{
+		LangVersion: "1.20",
+		ExtraRules:  true,
+		ModulePath:  "github.com/fardream/gmsk",
+	})
+	if err != nil {
+		os.Stdout.Write(fileContent.Bytes())
+		orPanic(err)
+	}
+
+	if outputDir != "" {
+		fullPath := path.Join(outputDir, outFile)
+		orPanic(os.WriteFile(fullPath, formattedContent, 0o644))
+	} else {
+		os.Stdout.Write(formattedContent)
+	}
 }
 
 func main() {
@@ -66,35 +88,9 @@ func main() {
 
 	orPanic(Normalize(m, config))
 
-	var enumFile bytes.Buffer
-
-	orPanic(BuildEnums(m, config, &enumFile))
-
-	fmtOpts := format.Options{
-		LangVersion: "1.20",
-		ExtraRules:  true,
-		ModulePath:  "github.com/fardream/gmsk",
-	}
-	formattedEnum := getOrPanic(format.Source(enumFile.Bytes(), fmtOpts))
-
-	if outputDir != "" {
-		enumOut := path.Join(outputDir, "enums.go")
-		orPanic(os.WriteFile(enumOut, formattedEnum, 0o644))
-	} else {
-		os.Stdout.Write(formattedEnum)
-	}
-
-	var resFile bytes.Buffer
-	orPanic(BuildResCode(m, config, &resFile))
-
-	formattedRes := getOrPanic(format.Source(resFile.Bytes(), fmtOpts))
-
-	if outputDir != "" {
-		resOut := path.Join(outputDir, "res", "codes.go")
-		orPanic(os.WriteFile(resOut, formattedRes, 0o644))
-	} else {
-		os.Stdout.Write(formattedRes)
-	}
+	builderToFile(outputDir, "enums.go", m, config, BuildEnums)
+	builderToFile(outputDir, path.Join("res", "codes.go"), m, config, BuildResCode)
+	builderToFile(outputDir, "funcs.go", m, config, BuildFuncs)
 
 	log.Printf("number of functions: %d", len(m.Functions))
 }
